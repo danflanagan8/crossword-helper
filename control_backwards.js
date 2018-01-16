1f88fa68d8caf8f2263b2e20d651156fa0bf0596
Dan Flanagan <dan.flanagan@manifest.com>
2018-01-16 16:47:26 -0600

    Add dedicated backwards versions.

---
+++ control_backwards.js
@@ -0,0 +1,218 @@
+//The control for Dan's Crossword Helper
+//uses object WordForest, WordTree, WildCardWord, and GridToFill
+//uses ajax.js library
+
+//dictionary is a global variable used by the controls
+var dictionary = new WordForest();
+
+//a file loading function based on http://blog.teamtreehouse.com/reading-files-using-the-html5-filereader-api
+function readInFile() {
+  var fileInput = document.getElementById('fileInput');
+  var fileDisplayArea = document.getElementById('fileDisplayArea');
+
+  fileInput.addEventListener('change', function(e) { 
+    var file = fileInput.files[0];
+    var textType = /text.*/;
+    
+    if (file.type.match(textType)) {
+  		var reader = new FileReader();   
+
+  		reader.onload = function(e) {
+    		//The stuff in here is now mine.
+  			//try to read the file line by line into an array
+  			var wordArray = reader.result.split(/[\n\r]/);
+  			
+  			//now we put the words into the word list randomly
+  			//it needs to be random since I use a binary tree.
+  			//It's slow but necessary.
+  			putWordsIntoDictionary(wordArray);
+  		}
+  	
+  	  reader.readAsText(file);  
+	
+	  } else {
+  		fileDisplayArea.innerText = "File not supported!";
+	  }
+  });
+}
+
+function putWordsIntoDictionary( wordArray ){
+
+	//this function builds the word forest.
+	//It adds the wordArray elements in a random order since the WordForest uses binary trees.
+	//Binary trees don't do well if we put in ordered elements.
+	
+	dictionary = new WordForest();
+
+	while( wordArray.length > 0 ){
+       
+       //get a random element
+       var rand_index = Math.floor( Math.random() * wordArray.length );	
+	   dictionary.add( reverseWord(wordArray[rand_index]) );
+	   //now get that element out of the array
+	   wordArray = wordArray.slice(0, rand_index).concat(wordArray.slice(rand_index+1));
+	   
+	}
+	
+}
+
+function reverseWord(str){
+  var rev = "";
+  for(var i = 1; i <= str.length; i++){
+    rev += str[str.length - i];
+  }
+  return rev;
+}
+
+function printMatchArray( matchArray ){
+	//this prints all of the possible matches below the wildWord input field
+	
+	var node = document.getElementById("wildWordMatches");
+
+	//clear the grid
+	while( node.firstChild ){
+		node.removeChild(node.firstChild);
+	}
+
+	if(matchArray.length == 0 ) matchArray.push("No matches found!");
+	matchArray.forEach( function(entry){
+		var text = document.createTextNode(reverseWord(entry) + "\n");
+		node.appendChild(text);
+	});
+
+}
+
+function addWord( word ){
+    
+    
+    if( word.indexOf("-") == -1 && word.indexOf("*") == -1 && word.indexOf("#") == -1 && word.indexOf("^") == -1){
+    
+       dictionary.add(reverseWord(word.toLowerCase()));
+    
+    }
+    
+}
+
+function removeWord( word ){
+    
+    
+    if( word.indexOf("-") == -1 && word.indexOf("*") == -1 && word.indexOf("#") == -1 && word.indexOf("^") == -1){
+    
+       dictionary.remove(reverseWord(word.toLowerCase()));
+    
+    }
+    
+}
+
+//by default, danswordlist.txt gets loaded by the page. The next two functions handle that along with the ajax.js library
+
+function loadDansWords(){
+
+  ajaxCallback = putDansWordsInDictionary;
+	ajaxRequest("danswordlist.txt");
+
+}
+
+function putDansWordsInDictionary(){
+
+   var list = ajaxreq.responseText;
+   var words = list.split(/[\n\r]/);
+   
+   words.forEach( function( entry ){
+       
+	   dictionary.add(reverseWord(entry));
+   
+   });
+
+}
+
+
+function findAndShowMatches(){
+
+    //handles the button in the Word Finder div
+
+    //get the pattern from the wildWord field
+    var to_match = new wildCardWord( reverseWord(document.getElementById('wildWord').value) );
+    
+    //get caret array from the caret field
+    var caret = [];
+   	var caret_string = document.getElementById("caret").value;
+   	for( var i = 0; i < caret_string.length ; i++ ){
+      	caret.push(caret_string[i]);
+  	}
+  	
+  	//get the tilde array from the tilde field
+  	var tilde = [];
+   	var tilde_string = document.getElementById("tilde").value;
+   	for( var i = 0; i < tilde_string.length ; i++ ){
+      	tilde.push(tilde_string[i]);
+  	}
+	
+	printMatchArray( to_match.getMatchArray( dictionary, caret, tilde ) );
+
+}
+
+function findAndShowGrid(){
+
+   //handles the button in the Grid Filler div (along with showSolution() )
+   var to_fill_string = document.getElementById("to_fill").value;
+   var words = to_fill_string.split("\n");
+
+   //rotate the words 180 degrees
+   words = rotateGrid(words);
+   //remove blank lines
+   for( var i = words.length - 1; i >= 0 ; i-- ){
+      if( words[i].length === 0 ){
+        words.splice(i, 1);
+      }
+   }
+   //check that all the lines are the same length
+   for( var i = 0; i < words.length; i++ ){
+      if( words[i].length !== words[0].length ){
+        document.getElementById("solution").value = "Error: not a rectangular grid";
+        return;
+      }
+   }   
+   var grid = new GridToFill( words );
+   document.getElementById("solution").value = "Working...";
+   setTimeout( function(){
+    var solution = grid.getSolution( dictionary );
+    solution = rotateGrid(solution);
+    showSolution( solution );
+    console.log( grid.solutions );
+   }, 10);
+}
+
+function rotateGrid(words){
+  if(!Array.isArray(words)){
+    return words;
+  }
+  var rotated = [];
+  for( var row = 0 ; row < words.length ; row++){
+    rotated[row] = "";
+    for( var col = 0; col < words[0].length ; col++){
+      rotated[row] += words[words.length - 1 - row][words[0].length - 1 - col];
+    }
+  }
+  console.log(rotated);
+  return rotated;
+}
+
+function showSolution(solution){
+
+   if( solution == null ){
+        document.getElementById("solution").value = "No solution found!";
+   }else{
+      var sol_str = ""
+      for( var i = 0 ; i < solution.length ; i++){
+         sol_str += (solution[i] + "\n");
+      }
+      document.getElementById("solution").value = sol_str.toUpperCase();
+   }
+  
+}
+
+
+
+
+
